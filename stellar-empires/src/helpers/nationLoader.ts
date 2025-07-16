2bf3fa897d5ad937b042dc3f13987b8418e76200
TCGM <thecrazygamemaster@gmail.com>
2025-07-16 11:25:57 -0700

    try fix for github pages

--- stellar-empires/src/helpers/nationLoader.ts
+++ stellar-empires/src/helpers/nationLoader.ts
@@ -1,10 +1,12 @@
 import { NationData, TraitOrFlaw } from "../types";
 
+const BASE = process.env.PUBLIC_URL || "";
+
 export async function fetchNationData(nationName: string): Promise<NationData> {
   const [nationRes, traitsRes, flawsRes] = await Promise.all([
-    fetch(`/data/nations/${nationName}/nation.json`),
-    fetch(`/data/nations/${nationName}/traits.json`),
-    fetch(`/data/nations/${nationName}/flaws.json`)
+    fetch(`${BASE}/data/nations/${nationName}/nation.json`),
+    fetch(`${BASE}/data/nations/${nationName}/traits.json`),
+    fetch(`${BASE}/data/nations/${nationName}/flaws.json`)
   ]);
   const [nationObj, traits, flaws] = await Promise.all([
     nationRes.json(),
@@ -20,7 +22,7 @@ export async function fetchNationData(nationName: string): Promise<NationData> {
 }
 
 export async function fetchAllNationData(): Promise<NationData[]> {
-  const res = await fetch("/data/nations/nationList.json");
+  const res = await fetch(`${BASE}/data/nations/nationList.json`);
   const nationNames: string[] = await res.json();
   const allData = await Promise.all(nationNames.map(n => fetchNationData(n)));
 //   console.log(JSON.stringify(allData));
